03f394a10652ce4e187f999006861ee932e3da08
Peter Marsh <pete.d.marsh@gmail.com>
2011-11-30 23:35:31 +0000

    Implemented getText() which gets all text on a web page

--- pybozo.py
+++ pybozo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import urllib2
 import BeautifulSoup
 import pygoogle
 
@@ -21,6 +22,26 @@ def search(hash):
 	
 	urls = [result.url for result in google.webSearch(str(hash))]
 	return urls
+	
+def getText(url):
+	"""Gets all of the text on a web page.
+	
+	Parameters
+		
+		url: str
+			The url of the web page to scrape
+	
+	Returns
+	
+		All of the text from the web page as a str
+	
+	"""
+
+	connection = urllib2.urlopen( str(url) )
+	soup = BeautifulSoup.BeautifulSoup( connection.read() )
+	tags = soup.findAll(text = True)
+	return tags
+	
 
 
 	
